f2681449552a64da2e1dc210db186539cd887384
Alexandru Dornescu <alexandrudornescu@yahoo.com>
2019-04-16 17:09:07 +0300

    Update. Rectangle move

--- main.py
+++ main.py
@@ -25,66 +25,52 @@ list_row_rect = []
 zz = 0
 x, y = 0, 0
 
-# def xx(COLOR):
-#     global x, y
-#     return pygame.draw.rect(screen, COLOR, [x, y, 20, 20], 1)
-
 for i in grid:
     for j in i:
-        xx = lambda COLOR: lambda COLOR: pygame.draw.rect(screen, COLOR, [x, y, 20, 20], 1)
-        list_row_rect.append(xx(RED))
-        x = x + 20
+        xx = pygame.draw.rect(screen, BLUE, [y, x, 20, 20], 1)
+        list_row_rect.append(xx)
+        y += 20
+    list_row_rect = []
     lista.append(list_row_rect)
-    x = 0
-    y = y + 20
-# print()
-x, y = 0, 0
-step = 20
+    x += 20
+    y = 0
+
+# TODO with functions
+
+len_lista = lista.__len__() - 1
+middle_screen = len_lista // 2
+
+x, y = middle_screen, middle_screen
+step = 1
 
 while 1:
     ok1 = 1
 
-    # for event in pygame.event.get():  # User did something
-    #     if event.type == pygame.QUIT:  # If user clicked close
-    #         done = True  # Flag that we are done so we exit this loop
-
     event = pygame.event.poll()
     if event.type == pygame.QUIT:
         break
-    # if (event.type == pygame.K_TAB):
-    #     pygame.draw.rect(screen, BLUE, [x, y, 20, 20])
-    #     y += 20
+
     if event.type == pygame.KEYDOWN:
+        print(x, y)
+        if (x == len_lista or y == len_lista) or (x < 0 or y < 0):
+            print("sfarsit")
+            # TODO rise exception
+
         if event.key == pygame.K_a:
-            x -= step
-            # pygame.draw.rect(screen, BLUE, [x, y, 20, 20])
-            lista[x][y](BLUE)
-        if event.key == pygame.K_d:
-            x += step
-            lista[x][y](BLUE)
-            # pygame.draw.rect(screen, BLUE, [x, y, 20, 20])
-        if event.key == pygame.K_w:
             y -= step
-            lista[x][y](BLUE)
-            # pygame.draw.rect(screen, BLUE, [x, y, 20, 20])
-        if event.key == pygame.K_s:
+            lista[x][y] = pygame.draw.rect(screen, RED, lista[x][y])
+            print(x, y)
+        if event.key == pygame.K_d:
             y += step
-            lista[x][y](BLUE)
-            # pygame.draw.rect(screen, BLUE, [x, y, 20, 20])
-    # pressed = pygame.key.get_pressed()d
-    # if pressed[pygame.K_w] and ok1:
-    #     print("w is pressed")
-    #     pygame.draw.rect(screen, BLUE, [x, y, 20, 20])
-    #     y += 20
-    #     ok1 = 0
-    #
-    # if pressed[pygame.K_s]:
-    #     print("s is pressed")
+            lista[x][y] = pygame.draw.rect(screen, RED, lista[x][y])
+        if event.key == pygame.K_w:
+            x -= step
+            lista[x][y] = pygame.draw.rect(screen, RED, lista[x][y])
+        if event.key == pygame.K_s:
+            x += step
+            lista[x][y] = pygame.draw.rect(screen, RED, lista[x][y])
 
     pygame.display.flip()
 
 # Be IDLE friendly
 pygame.quit()
-
-# if __name__ == '__main__':
-#
