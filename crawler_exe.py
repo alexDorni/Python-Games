8347a9c2bd7db47ef3dabb0afc59740028cb4b8b
alexDorni <alexandrudornescu@yahoo.com>
2019-05-06 22:03:50 +0300

    Update console

--- crawler_exe.py
+++ crawler_exe.py
@@ -28,7 +28,5 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Web crawler")
     parser.add_argument("--links_file", help="A .txt file wich contains links to be crawled", default='')
     parser.add_argument("--nr_threads", help="Number of threads your PC can handle", type=int, default=8)
-    parser.add_argument("--key_word", help="Search in links a specific key_word", default=None)
-    parser.add_argument("--plot", help="Plot the rate of words in links")
     args = parser.parse_args()
     main(args)
